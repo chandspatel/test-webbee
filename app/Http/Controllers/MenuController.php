20b62f9596e795b4f49ed75ff6d0d95e014223a4
chands <chandspatel888@gmail.com>
2022-06-07 00:04:28 +0530

    get all child menu

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -95,6 +95,7 @@ class MenuController extends BaseController
      */
 
     public function getMenuItems() {
-        throw new \Exception('implement in coding task 3');
+        $mainMenu = MenuItem::with('allChildren')->whereNull('parent_id')->get();
+        return \Response::json($mainMenu->toArray());
     }
 }
--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -9,4 +9,14 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
 
+
+
+	public function children() {
+		return $this->hasMany('\App\Models\MenuItem', 'parent_id');
+	}
+
+	public function allChildren()
+	{
+		return $this->children()->with('allChildren');
+	}
 }
